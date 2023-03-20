fab3745ac967c9b2c0fc3f6d7b0e95a37d0111a3
kaayDev <jugalrajshrestha8@gmail.com>
2023-03-20 16:06:15 +0545

    working on favourites

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "../typography/text.component";
+import { restaurantRequest } from "../../services/restaurants/restaurants.service";
+
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebView : CompactImage;
+  return (
+    <Item>
+      <Image source={{ uri: restaurantRequest.photos[0] }} />
+      <Text variant="caption">{restaurantRequest.name}</Text>
+    </Item>
+  );
+};
